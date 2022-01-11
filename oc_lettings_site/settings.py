440c6da6c3216eb405d395c339223cafdfa10541
Olivier SAMIN <oliviersamin@gmail.com>
2022-01-11 22:52:47 +0000

    update settings.py with whitenoise to get static files in production for Heroku

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "test-oc-p13.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "oc-lettings-os.herokuapp.com"]
 
 
 # Application definition
@@ -39,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'oc_lettings_site.urls'

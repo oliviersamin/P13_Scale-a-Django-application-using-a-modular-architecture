e308ecd0a7c16c279aa8e941341146d9f4b670d7
Olivier SAMIN <oliviersamin@gmail.com>
2022-01-18 11:20:31 +0000

    update

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 
 sentry_sdk.init(
     # dsn=config('SENTRY_DSN'),
-    dsn=os.environ['SENTRY_DSN']
+    dsn = os.environ['SENTRY_DSN'],
     # dsn="https://4acffdc0554c4d44a80f6ab06e5c0d7b@o1114418.ingest.sentry.io/6145602",
     integrations=[DjangoIntegration()],
 
